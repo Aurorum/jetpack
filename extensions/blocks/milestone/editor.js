ba5226cbac264590d35dc0fa8ee5536b513d2655
torres126 <torres126@clubpenguinmountains.com>
2019-04-10 22:49:10 +0300

    Create editor.js

---
+++ extensions/blocks/milestone/editor.js
@@ -0,0 +1,8 @@
+/**
+ * Internal dependencies
+ */
+
+import registerJetpackBlock from '../../shared/register-jetpack-block';
+import { name, settings } from '.';
+
+registerJetpackBlock( name, settings );
